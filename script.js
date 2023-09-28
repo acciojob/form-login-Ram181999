afbffeed0fa237ca1f3668ddd08de746de93024a
Ram181999 <141997273+Ram181999@users.noreply.github.com>
2023-09-28 20:29:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+	 const fname = document.getElementsByTagName("input")[0];
+   const lname = document.getElementsByTagName("input")[1];
+  alert(fname.value +" "+lname.value);
 
 }
